de11afad4e783b348cd153456d01c754e2675797
haley-mcquick <72761290+haley-mcquick@users.noreply.github.com>
2020-11-29 17:17:26 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ light.set_brightness(20)
 tempF = input.temperature(TemperatureUnit.FAHRENHEIT)
 print ("Temperature:" + tempF + "F")
 
-if tempF >= 38 and  tempF <= 40:
-    light.set_all(color.rgb(0,255,0))
+# if tempF >= 38 and  tempF <= 40:
+#     light.set_all(color.rgb(0,255,0))
+# elif tempF < 38:
+#     light.set_all(color.rgb(0, 0, 255))
+# else:
+#     light.set_all(color.rgb(255,0,0))
+
+if tempF > 37 and  tempF < 41:
+    light.set_all(color.rgb(255,255,0))
+    print ("Temperature:" + tempF + "F")
 elif tempF < 38:
-    light.set_all(color.rgb(0, 0, 255))
+    light.set_all(color.rgb(0, 255, 255))
+    print ("Temperature:" + tempF + "F")
 else:
-    light.set_all(color.rgb(255,0,0))
-    
+    light.set_all(color.rgb(255,0,255))
+    print ("Temperature:" + tempF + "F")
+      
--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
 light.setBrightness(20)
 let tempF = input.temperature(TemperatureUnit.Fahrenheit)
 console.log("Temperature:" + tempF + "F")
-if (tempF >= 38 && tempF <= 40) {
-    light.setAll(color.rgb(0, 255, 0))
+//  if tempF >= 38 and  tempF <= 40:
+//      light.set_all(color.rgb(0,255,0))
+//  elif tempF < 38:
+//      light.set_all(color.rgb(0, 0, 255))
+//  else:
+//      light.set_all(color.rgb(255,0,0))
+if (tempF > 37 && tempF < 41) {
+    light.setAll(color.rgb(255, 255, 0))
+    console.log("Temperature:" + tempF + "F")
 } else if (tempF < 38) {
-    light.setAll(color.rgb(0, 0, 255))
+    light.setAll(color.rgb(0, 255, 255))
+    console.log("Temperature:" + tempF + "F")
 } else {
-    light.setAll(color.rgb(255, 0, 0))
+    light.setAll(color.rgb(255, 0, 255))
+    console.log("Temperature:" + tempF + "F")
 }
 
